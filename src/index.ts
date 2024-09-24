5773050e7f2904af7be7427129869de6db25423a
kirubel12 <kirubelhabte1991@gmail.com>
2024-09-24 14:03:01 +0300

    modified

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello Hono!",
+  });
+});
+
 app.get("/hello", (c) => {
   return c.json({
     message: "Hello Next.js!",
